21ab4e60ed8abeba56b95da3d28859a1704b556a
cfue <curtis.ch.chat@gmail.com>
2020-06-10 23:13:24 -0600

    avoiding duplication of config

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-createAutoComplete({
-  root: document.querySelector(".autocomplete"),
+const autoCompleteConfig = {
   renderOption(movie) {
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
     return `
@@ -27,6 +26,17 @@ createAutoComplete({
 
     return response.data.Search;
   },
+};
+
+createAutoComplete({
+  ...autoCompleteConfig,
+  // ... means make a copy of all the functions and throw it in here
+  root: document.querySelector("#left-autocomplete"),
+});
+createAutoComplete({
+  ...autoCompleteConfig,
+  // ... means make a copy of all the functions and throw it in here
+  root: document.querySelector("#right-autocomplete"),
 });
 
 const onMovieSelect = async (movie) => {
