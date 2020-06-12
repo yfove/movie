3d674a88c2f2b91f4d9bd9badfe670f8a7eb25d6
cfue <curtis.ch.chat@gmail.com>
2020-06-12 14:23:17 -0600

    run comparison function

--- index.js
+++ index.js
@@ -68,11 +68,50 @@ const onMovieSelect = async (movie, summaryElement, side) => {
   }
 
   const runComparison = () => {
-    console.log("Time for comparison");
+    const leftSideStats = document.querySelectorAll(
+      "#left-summary .notification"
+    );
+    const rightSideStats = document.querySelectorAll(
+      "#right-summary .notification"
+    );
+
+    leftSideStats.forEach((leftStat, index) => {
+      const rightStat = -rightSideStats[index];
+
+      const leftSideValue = leftStat.dataset.value;
+      const rightSideValue = rightStat.dataset.value;
+
+      if (rightSideValue > leftSideValue) {
+        leftStat.classList.remove("is-primary");
+        leftStat.classList.add("is-warning");
+      } else {
+        rightStat.classList.remove("is-primary");
+        rightStat.classList.add("is-warning");
+      }
+    });
+    console.log(leftStat, rightStat);
   };
 };
 
 const movieTemplate = (movieDetail) => {
+  // '$629,000,000'
+  // const dollars = parseInt(
+  //   movieDetail.BoxOffice.replace(/\$/g, "").replace(/,/g, "")
+  // );
+  const metascore = parseint(movieDetail.Metascore);
+  const imbdRating = parseFloat(movieDetail.imdbRating);
+  const imbdVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""));
+  const awards = movieDetail.awards.split("").reduce((prev, word) => {
+    const value = parseInt(word);
+    if (isNaN(value)) {
+      return prev;
+    } else {
+      return prev + value;
+    }
+  }, 0);
+
+  console.log(awards, metascore, imdbRating, imdbVotes);
+
   return `
   <article class="media">
   <figure class="media-left">
@@ -88,19 +127,20 @@ const movieTemplate = (movieDetail) => {
     </div>
   </div>
 </article>
-<article class="notification is-primary">
+
+<article data-value=${awards} class="notification is-primary">
   <p class="title">${movieDetail.Awards}</p>
   <p class="subtitle">Awards</p>
 </article>
-<article class="notification is-primary">
+<article data-value=${metascore} class="notification is-primary">
   <p class="title">${movieDetail.Metascore}</p>
   <p class="subtitle">Meta Critic Score</p>
 </article>
-<article class="notification is-primary">
+<article data-value=${imdbRating} class="notification is-primary">
   <p class="title">${movieDetail.imdbRating}</p>
   <p class="subtitle">imdb Rating</p>
 </article>
-<article class="notification is-primary">
+<article data-value=${imdbVotes} class="notification is-primary">
   <p class="title">${movieDetail.imdbVotes}</p>
   <p class="subtitle">imdb Votes</p>
 </article>
